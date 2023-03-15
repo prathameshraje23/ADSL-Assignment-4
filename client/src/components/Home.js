c13ed3a713c3e9e1fa6043ac682ac63c6fe384e8
Prathamesh Raje <prathameshraje23@gmail.com>
2023-03-15 10:17:44 +0530

    first commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+
+  return (
+    <>
+      <h1 className='title'>MyQuiz</h1>
+      <img className='title-img' src="https://www.riddle.com/imageservice/q_80,f_auto,c_fill,w_960,h_540/ed6cozjawexldjs7yedn" alt="abc" />
+    </>
+  )
+}
+
+export default Home
+
+
+
+
